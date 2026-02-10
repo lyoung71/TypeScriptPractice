d6b0a66f1f5f7e2ecf968ccc178370cb788d0b4a
Landon Young <landonyoung71@gmail.com>
2026-02-10 15:42:00 -0600

    Creating classes

---
+++ 04-classes/01-class-definition/Customer.js
@@ -0,0 +1,10 @@
+var Customer = /** @class */ (function () {
+    function Customer() {
+    }
+    return Customer;
+}());
+var myCustomer = new Customer();
+myCustomer.firstName = "Landon";
+myCustomer.lastName = "Young";
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
---
+++ 04-classes/01-class-definition/Customer.ts
@@ -0,0 +1,15 @@
+class Customer {
+
+    firstName: string;
+    lastName: string;
+
+
+}
+
+let myCustomer: Customer = new Customer();
+myCustomer.firstName = "Landon";
+myCustomer.lastName = "Young";
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
+
