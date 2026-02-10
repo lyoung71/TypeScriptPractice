8fa0c6c753ba10b967ca00870bae27ae2ce6e83e
Landon Young <landonyoung71@gmail.com>
2026-02-10 15:24:46 -0600

    growable arrays

---
+++ 03-loops-and-arrays/growable-arrays.js
@@ -0,0 +1,7 @@
+var sportsArray2 = ["Golf", "Tennis", "Bowling", "Swimming"];
+sportsArray2.push("Volleyball");
+sportsArray2.push("Soccer");
+for (var _i = 0, sportsArray2_1 = sportsArray2; _i < sportsArray2_1.length; _i++) {
+    var sport = sportsArray2_1[_i];
+    console.log(sport);
+}
---
+++ 03-loops-and-arrays/growable-arrays.ts
@@ -0,0 +1,8 @@
+let sportsArray2: string [] = ["Golf", "Tennis", "Bowling", "Swimming"];
+
+sportsArray2.push("Volleyball");
+sportsArray2.push("Soccer");
+
+for (let sport of sportsArray2) {
+    console.log(sport);
+}
